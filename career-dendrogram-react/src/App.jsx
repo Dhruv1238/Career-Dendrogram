b88376599cb84e3618b0f2cfa42293df161d056e
Pratham Goenka <prathamg1803@gmail.com>
2023-04-26 02:16:58 +0530

    made navbar

--- career-dendrogram-react/src/App.jsx
+++ career-dendrogram-react/src/App.jsx
@@ -3,15 +3,13 @@ import './App.css'
 import Navbar  from "./components/navbar/navbar.jsx";
 
 
-
-
 function App() {
   return (
     <>
     <Navbar />
-    <h1 className="text-3xl font-bold underline">
+    {/* <h1 className="text-3xl font-bold underline">
       Hello world!
-    </h1>
+    </h1> */}
     </> 
   )
 }
