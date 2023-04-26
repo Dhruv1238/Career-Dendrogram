0c2fa78ee1a4a1e3ed3fc68814c28782aae2d8c2
Dhruv Sharma <dhruv4075@gmail.com>
2023-04-27 02:43:53 +0530

    Api takes user input

--- career-dendrogram-react/src/App.jsx
+++ career-dendrogram-react/src/App.jsx
@@ -7,27 +7,57 @@ import  generateText  from './components/gpt/gpt.jsx'
 
 
 function App() {
-
+  const [typing,setTyping]=useState(false);
+  const [interests, setInterests] = useState('');
   const [text, setText] = useState('');
-  
-  async function handleClick() {
-    const interests="Computer, IT"
-    const prompt = `Based on these interests: ${interests}, here are some career options:`;
-    const generatedText = await generateText(prompt);
-    setText(generatedText);
-    console.log("clicked");
-  }
 
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setTyping(true);
+    const prompt = `Based on these interests: ${interests}, here is the list of 5 career options:`;
+    console.log(prompt);
+    const generatedText = await generateText(prompt);
+    console.log(generatedText);
+  
+    if (generatedText) {
+      setText(generatedText);
+      setTyping(false);
+    } else {
+      console.error("Empty text output");
+      setTyping(false);
+    }
+  }
+  
   return (
     <>
     <Navbar />
     <div>
       <h1>GPT-3 Career Generator</h1>
-      <button onClick={handleClick}>Generate Text</button>
-      <p>{text}</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={interests}
+          onChange={(e) => setInterests(e.target.value)}
+          placeholder="Enter your interests"
+        />
+        <button type="submit">Generate</button>
+      </form>
+      <div type="text-output">
+        {typing ? "Generating..." : null}
+      </div>
+      <div className="text-output">
+        <p>{text}</p>
+      </div>
     </div>
     </> 
   )
 }
 
 export default App
+
+  // async function handleSubmit() {
+  //   const prompt = `Based on these interests: ${interests}, here are some career options:`;
+  //   const generatedText = await generateText(prompt);
+  //   setText(generatedText);
+  //   console.log("clicked");
+  // }
